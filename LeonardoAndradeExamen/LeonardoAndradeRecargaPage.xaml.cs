614a50f51aff94014a170248a0d2644328b1e60f
Leonardo <leonardoandrade2003.197@gmail.com>
2024-11-29 07:49:42 -0500

    Tabbed page recarga diseño y tabbed page nombres completos Leonardo Andrade diseño falta la lógica

--- LeonardoAndradeExamen/LeonardoAndradeRecargaPage.xaml.cs
+++ LeonardoAndradeExamen/LeonardoAndradeRecargaPage.xaml.cs
@@ -1,9 +1,15 @@
-namespace LeonardoAndradeExamen;
-
-public partial class LeonardoAndradeRecargaPage : ContentPage
+namespace LeonardoAndradeExamen
 {
-	public LeonardoAndradeRecargaPage()
-	{
-		InitializeComponent();
-	}
+    public partial class LeonardoAndradeRecargaPage : ContentPage
+    {
+        public LeonardoAndradeRecargaPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnRecargarClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Éxito", "Recarga realizada correctamente.", "OK");
+        }
+    }
 }
